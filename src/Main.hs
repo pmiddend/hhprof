faf4d9998710094494eb9564f12943e537486ffc
Philipp Middendorf <pmidden@secure.mailbox.org>
2016-11-24 17:53:43 +0100

    commit before parsing heap dumps

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,6 @@
 module Main where
 
-import Prelude(fromIntegral,(-))
+import Prelude(fromIntegral,(-),undefined)
 import Data.List(intercalate)
 import Data.String(String)
 import Data.Either(Either(..))
@@ -12,7 +12,7 @@ import Data.Monoid((<>))
 import qualified Data.ByteString as BS
 import Data.Word(Word8,Word16,Word32,Word64)
 import Data.Attoparsec.ByteString(Parser,takeWhile,count,take,parseOnly,anyWord8,many')
-import Data.Attoparsec.Binary(anyWord32be,anyWord64be)
+import Data.Attoparsec.Binary(anyWord32be,anyWord64be,anyWord16be)
 import Control.Monad(return)
 import Control.Applicative((<*>))
 import Data.Functor((<$>))
@@ -35,6 +35,12 @@ headerParser = do
   dateLow <- to64 <$> anyWord32be
   return $ Header intro identifierSize (shift dateHigh 32 .|. dateLow)
 
+siteParser :: Parser AllocSite
+siteParser = AllocSite <$> anyWord8 <*> anyWord32be <*> anyWord32be <*> anyWord32be <*> anyWord32be <*> anyWord32be <*> anyWord32be
+
+traceParser :: Parser Trace
+traceParser = Trace <$> anyWord32be <*> anyWord32be
+
 tagParser :: Word32 -> Parser a -> Parser (Tag a)
 tagParser idLength idParser = do
   tagType <- anyWord8
@@ -54,22 +60,54 @@ tagParser idLength idParser = do
       numberOfFrames <- anyWord32be
       frameIds <- count (fromIntegral numberOfFrames) idParser
       return $ TagStackTrace stackTraceSerialNumber threadSerialNumber numberOfFrames frameIds
+    0x06 -> do
+      bitMaskFlags <- anyWord16be
+      cutoffRatio <- anyWord32be
+      liveBytes <- anyWord32be
+      liveInstances <- anyWord32be
+      bytesAllocated <- anyWord64be
+      instancesAllocated <- anyWord64be
+      siteCount <- anyWord32be
+      sites <- count (fromIntegral siteCount) siteParser
+      return $ TagAllocSites bitMaskFlags cutoffRatio liveBytes liveInstances bytesAllocated instancesAllocated siteCount sites
+    0x07 -> TagHeapSummary <$> anyWord32be <*> anyWord32be <*> anyWord64be <*> anyWord64be
+    0x0A -> do
+      threadSerialNumber <- anyWord32be
+      threadObjectId <- idParser
+      traceSerialNumber <- anyWord32be
+      threadNameStringId <- idParser
+      threadGroupNameId <- idParser
+      parentGroupNameId <- idParser
+      return $ TagStartThread threadSerialNumber threadObjectId traceSerialNumber threadNameStringId threadGroupNameId parentGroupNameId
+    0x0B -> TagEndThread <$> anyWord32be
+    0x2C -> return TagHeapDumpEnd
+    0x0D -> do
+      numberSamples <- anyWord32be
+      tracesCount <- anyWord32be
+      traces <- count (fromIntegral tracesCount) traceParser
+      return $ TagCpuSamples numberSamples tracesCount traces
+    0x0E -> TagControlSettings <$> anyWord32be <*> anyWord16be
+    0x0C -> parseHeapDump idParser
+    0x1C -> parseHeapDump idParser
     _ -> do
       _ <- take (fromIntegral length)
       return TagDummy
+
+parseHeapDump :: Parser a -> Parser (Tag a)
+parseHeapDump = undefined
 
 data Tag a = TagUtf8String a BS.ByteString
            | TagLoadClass Word32 a Word32 a
            | TagUnloadClass Word32
            | TagStackFrame a a a a Word32 Word32
            | TagStackTrace Word32 Word32 Word32 [a]
-           | TagAllocSites Word16 Word32 Word32 Word32 Word64 Word64 [AllocSite]
+           | TagAllocSites Word16 Word32 Word32 Word32 Word64 Word64 Word32 [AllocSite]
            | TagHeapSummary Word32 Word32 Word64 Word64
            | TagStartThread Word32 a Word32 a a a
            | TagEndThread Word32
            | TagHeapDump [SubTag a]
            | TagHeapDumpEnd
-           | TagCpuSamples Word32 [Trace]
+           | TagCpuSamples Word32 Word32 [Trace]
            | TagControlSettings Word32 Word16
            | TagDummy deriving(Show)
 
